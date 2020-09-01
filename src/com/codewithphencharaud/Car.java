a168fab9a74b418716083dcd75294199b383a7e9
Phen Thach <pshannonthach@gmail.com>
2020-09-01 18:02:34 -0400

    added methods and sub class to my car java project

--- src/com/codewithphencharaud/Car.java
+++ src/com/codewithphencharaud/Car.java
@@ -1,11 +1,41 @@
 package com.codewithphencharaud;
 
 public class Car {
-    String color;
-    String make;
-    String model;
+    private String make;
+    private String model;
+    private int year;
 
-    public String getInfo() {
-        return ("My car model is "+model+", make is "+make+", color is "+color);
+    public Car(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
     }
+
+    public void honk(){
+        System.out.println("Honk! Honk!");
+    }
+
+    public void seatBeltAlert(){
+        System.out.println("Beep! Beep! Beep! Beep!");
+    }
+    public String getCarInfo() {
+        return ("My car model is "+model+", make is "+make+", year is "+year);
+    }
+}
+
+class GasPriceForMyCar extends Car {
+    private double gasPrice;
+    private int tankGallon;
+
+    public GasPriceForMyCar (double gasPrice, int tankGallon, String make, String model, int year){
+        super(make, model, year);
+        this.gasPrice = gasPrice;
+        this.tankGallon = tankGallon;
+    }
+
+    public double setTotalCostForGas(){
+        return gasPrice*tankGallon;
+    }
+
+
 }
--- src/com/codewithphencharaud/Main.java
+++ src/com/codewithphencharaud/Main.java
@@ -3,10 +3,11 @@ package com.codewithphencharaud;
 public class Main {
 
     public static void main(String[] args) {
-        Car myCar = new Car();
-        myCar.color = "white";
-        myCar.model = "toyota";
-        myCar.make = "corolla";
-        System.out.println(myCar.getInfo());
+        Car myCar = new Car("Toyota", "Corolla", 2018);
+        GasPriceForMyCar totalCostForGas = new GasPriceForMyCar(2.12, 11, "Toyota", "Corolla", 2018);
+        myCar.seatBeltAlert();
+        myCar.honk();
+        System.out.println(myCar.getCarInfo());
+        System.out.print("The cost to fill up my gas tank is $"+totalCostForGas.setTotalCostForGas());
     }
 }
